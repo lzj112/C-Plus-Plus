37f519588556ab8c0e80c93887fe86ed7b66ddb0
lzj112 <872575628@qq.com>
2019-03-21 20:31:06 +0800

    .

--- test.cpp
+++ test.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
-#include <array>
-#include <unistd.h>
-#include <thread>
-#include <mutex>
+#include <vector>
+#include <list>
+#include <queue>
+#include <memory>
+#include <string>
+#include <algorithm>
 using namespace std;
+
+struct listNode
+{
+    listNode(int v) : val(v) {}
+    int val;
+    listNode* next;
+};
+
+listNode* func(listNode* root) 
+{
+    if (root == nullptr)
+        return nullptr;
+    listNode* preNode = nullptr;
+    listNode* nextNode = root->next;
+
+    while (root != nullptr) 
+    {
+        root->next = preNode;
+        preNode = root;
+        root = nextNode;
+        if (nextNode != nullptr)
+            nextNode = nextNode->next;
+    }
+    return preNode;
+}
+
 int main() 
 {
-    pid_t pid = fork();
-    if (pid == 0) 
-        printf("son\n");
-    else 
-        printf("father\n");
+    listNode* root = new listNode(1);
+    root->next = new listNode(2);
+    root->next->next = new listNode(3);
+
+    listNode* r = func(root);
+
+    while (r != nullptr)
+    {
+        cout << r->val << endl;
+        r = r->next;
+    }
 }
