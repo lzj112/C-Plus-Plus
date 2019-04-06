7afff71b1b169b6cf6d68a0f3a0b3f941be3ac30
lzj112 <872575628@qq.com>
2019-04-06 20:04:30 +0800

    .

--- test.cpp
+++ test.cpp
@@ -4,24 +4,24 @@
 #include <memory>
 #include <utility>
 #include <iostream>
+#include <typeinfo>
 #include <unordered_set>
 #include <unordered_map>
 using namespace std;
 
-int main()
-{
 
-    auto ptr1 = make_shared<int> (5);
-    auto ptr2 = ptr1;   
-    unordered_set<shared_ptr<int> > s1;
-    s1.insert(ptr1);
-    s1.insert(ptr2);
-    cout << s1.size() << endl;
-    
-    int* ptr_1 = new int(10);
-    int* ptr_2 = ptr_1;
-    unordered_set<int *> s2;
-    s2.insert(ptr_1);
-    s2.insert(ptr_2);
-    cout << s2.size() << endl;
+class test 
+{
+public:
+    test(int aa) : a(aa)
+    {}
+private:
+    int a;
+};
+
+int main() 
+{
+    test t(199);
+    int* ptr = (int *)&t;
+    cout << *ptr << endl;
 }
